f5a7a97605b513fb578852e09886589f5449d3a2
yerim <hongmm23@gmail.com>
2018-05-25 20:04:30 +0900

    파이어베이스 세팅

---
+++ GlassShoes/GlassShoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlassShoes
+//
+//  Created by software_studio1 on 2018. 5. 25..
+//  Copyright © 2018년 software_studio1_예림. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GlassShoes/Pods/Target Support Files/Pods-GlassShoesUITests/Pods-GlassShoesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GlassShoesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GlassShoesUITests
+@end
